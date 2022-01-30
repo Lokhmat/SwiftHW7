0584a2a129b84ff342e415378e9301df3fc9ba74
Sergey Lokhmatikov <lohmat@yandex-team.ru>
2022-01-30 15:10:36 +0300

    Initial Commit

---
+++ splokhmatikovPW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splokhmatikovPW7
+//
+//  Created by Sergey Lokhmatikov on 30.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
